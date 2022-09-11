fcda0d1b87151c65498eb203f0a606aa6016c297
karajan1001 <mishanyo1001@gmail.com>
2022-09-11 12:00:38 +0800

    Drop python3.6 support
    fix: #65

--- setup.py
+++ setup.py
@@ -18,15 +18,16 @@ setup(
     install_requires=["fsspec>=2021.7.0", "oss2>=2.6.1"],
     extras_require={"tests": tests_requirements},
     keywords="oss, fsspec",
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=find_packages(exclude=["tests"]),
 )
