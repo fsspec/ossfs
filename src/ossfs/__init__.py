5196c28dda7c09ce5b8ec0c3fa41e019d674bf72
karajan1001 <mishanyo1001@gmail.com>
2023-04-15 20:01:51 +0800

    Seperate OSSFile from core to file.py
    fix #91
    1. As the project gets more and more complex, can not keep All things in a
    single file.
    2. Update some typing hints.

--- src/ossfs/__init__.py
+++ src/ossfs/__init__.py
@@ -3,6 +3,7 @@ OSSFS
 ----------------------------------------------------------------
 A pythonic file-systems interface to OSS (Object Storage Service)
 """
-from .core import OSSFile, OSSFileSystem
+from .core import OSSFileSystem
+from .file import OSSFile
 
 __all__ = ["OSSFile", "OSSFileSystem"]
