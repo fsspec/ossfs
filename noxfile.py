5475375a0924b39092bf4be5b03d1aca2838353b
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2025-01-30 12:13:48 +0545

    update mypy and run nox tests upto 3.13

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,9 @@ nox.options.sessions = "lint", "tests"
 locations = "src", "tests"
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "pypy3.8", "pypy3.9"])
+@nox.session(
+    python=["3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "pypy3.8", "pypy3.9"]
+)
 def tests(session: nox.Session) -> None:
     session.install(".[tests]")
     session.run(
